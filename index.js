999c4c8ec87d0d242c718e120562c25278f4ab20
italoviola <italo.viola@fbiz.com.br>
2019-10-06 18:18:59 -0300

    Adjust setup + intiate institutional page creation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('static/html'));
+app.use(express.static('static/'));
+// app.set('engine', 'mustache');
 
 app.listen(3000, function() {
     console.log('Listening port 3000');
