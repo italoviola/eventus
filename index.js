18ed9363d6d6e1c6a0de7c678c4f31070d29540d
italoviola <italo.viola@fbiz.com.br>
2019-11-10 19:45:53 -0300

    Minor adjusts

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const site = express();
-const Mustache = require("mustache");
-const request = require("./static/js/request.js")
 
 site.use(express.static('static/'));
-// site.set('engine', 'mustache');
 
 site.listen(3000, function() {
     console.log('Listening port 3000');
-});
-
-site.get('/request', function(req, res) {
-    res.send(request);
 });
--- static/js/request.js
+++
@@ -1,9 +0,0 @@
-var RequestClass = function() {
-    getList()
-}; 
-
-RequestClass.prototype.getList = function() {
-    return "My List";
-};
-
-exports.Request = RequestClass;
