40e4567a6d01e901fbb623772c33bc2009450923
italoviola <italo.viola@fbiz.com.br>
2019-11-10 23:01:06 -0300

    Add name to localStorage

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,28 @@
 // Storage functions
 
+// Set
 function storageSetEmail(email) {
     localStorage.setItem('EventusEmail', email)
 }
 
+function storageSetName(name) {
+    localStorage.setItem('EventusName', name)
+}
+
+//Get
 function storageGetEmail() {
     return localStorage.getItem('EventusEmail')
 }
 
+function storageGetName() {
+    return localStorage.getItem('EventusName')
+}
+
+//Remove
 function storageRemoveEmail() {
     localStorage.removeItem('EventusEmail')
+}
+
+function storageRemoveName() {
+    localStorage.removeItem('EventusName')
 }
